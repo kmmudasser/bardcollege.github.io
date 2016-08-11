991deb7b4c675304305eb1d9f800ac51ab433181
kmmudasser <km2128@bard.edu>
2016-08-11 15:22:01 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -29,7 +29,7 @@
 
 var rules = {
   "~*x hello ~*y": [
-    "How do you do. Please state your problem."
+    "How do you do Mudasser?."
   ],
   "~*x computer ~*y": [
     "Do computers worry you?",
